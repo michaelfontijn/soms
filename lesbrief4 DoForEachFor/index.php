d3de8d15d0447974a7c2c75f477507969637d0f5
michaelfontijn <mich207hael@hotmail.com>
2016-02-24 12:25:49 +0100

    sooooooms

--- lesbrief4 DoForEachFor/index.php
+++ lesbrief4 DoForEachFor/index.php
@@ -20,19 +20,58 @@ $productCalorien = Array(
 )
 ?>
 
-    <?php foreach ($productCalorien as $product => $cal) :?>
-        <?php
+    <?php foreach ($productCalorien as $product => $cal)
+    {
         echo "In het product " . $product . " zitten " . $cal . " calorieEn" . "<br>";
-        ?>
-    <?php endforeach ?>
+    }
+    ?>
 
 
 <h2>opdracht 3</h2>
 <?php
 $i = 2012;
 do{
-    echo "$i";
+    echo "$i", "<br>";
     $i += 4;
 }
 while($i <= 2084);
 ?>
+
+<h2>opdracht 4</h2>
+<?php
+$auto = Array(
+    "audi" => Array(
+        "Naam" => "Audo A6",
+        "Max snelheid" => "240 km/h",
+        "verbruik" => "12"
+    ),
+    "porsche" => Array(
+        "Naam" => "Porsche 911",
+        "Max snelheid" => "290 km/h",
+        "verbruik" => "8"
+    ),
+    "fiat" => Array(
+        "Naam" => "Fiat panda",
+        "Max snelheid" => "140 km/h",
+        "verbruik" => "22"
+    )
+
+);
+
+foreach ($auto as $car ) {
+    echo "Naam: " . $car['Naam'];
+    if($car['verbruik'] > 11)
+    {
+        echo "(lekker zuinig)";
+    }
+    echo "<br>";
+    echo "Max snelheid: " . $car['Max snelheid'];
+    echo "<br>";
+    echo "Verbruik: " . " 1 op " . $car['verbruik'];
+    echo "<br>";
+    echo "<br>";
+}
+
+?>
+
+
